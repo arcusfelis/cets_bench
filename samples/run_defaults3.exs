f7b854270e632788bad856679e288bc65aac67b3
Mikhail Uvarov <arcusfelis@gmail.com>
2023-07-30 13:35:41 +0200

    update samples

--- samples/run_defaults3.exs
+++ samples/run_defaults3.exs
@@ -3,17 +3,18 @@
 {:ok, _peer, node} = :peer.start(%{:name => :bench2})
 :ok = :rpc.call(node, :code, :add_paths, [:code.get_path()])
 
-:bench = :rpc.block_call(node, :ets, :new, [:bench, [:named_table, :public]])
-:bench = :ets.new(:bench, [:named_table, :public])
+{:ok, _peer, node3} = :peer.start(%{:name => :bench3})
+:ok = :rpc.call(node3, :code, :add_paths, [:code.get_path()])
 
 # Init mnesia
 :mnesia.start()
 :ok = :rpc.call(node, :mnesia, :start, [])
-:mnesia.change_config(:extra_db_nodes, [node])
+:ok = :rpc.call(node3, :mnesia, :start, [])
+{:ok, _} = :mnesia.change_config(:extra_db_nodes, [node, node3])
 
 {:atomic, :ok} = :mnesia.create_table(
     :user,
-   [{:ram_copies, [node(), node]}, 
+   [{:ram_copies, [node(), node, node3]}, 
      attributes: [:id, :name]
    ])
 
@@ -21,29 +22,32 @@
 
 {:ok, pid1} = :cets.start(:cets_user, %{})
 {:ok, pid2} = :rpc.call(node, :cets, :start, [:cets_user, %{}])
+{:ok, pid3} = :rpc.call(node3, :cets, :start, [:cets_user, %{}])
 :ok = :cets_join.join(:join_lock1, %{}, pid1, pid2)
+:ok = :cets_join.join(:join_lock1, %{}, pid1, pid3)
 
 
 # Init CETS
 
 {:ok, pid1_new} = :cets.start(:cets_user_new, %{})
 {:ok, pid2_new} = :rpc.call(node, :cets, :start, [:cets_user_new, %{}])
+{:ok, pid3_new} = :rpc.call(node3, :cets, :start, [:cets_user_new, %{}])
 :ok = :cets_join.join(:join_lock1, %{}, pid1_new, pid2_new)
+:ok = :cets_join.join(:join_lock1, %{}, pid1_new, pid3_new)
 
 newId = fn -> :erlang.unique_integer([:positive]) end
 
 Benchee.run(
   %{
-    "ets" => fn -> :true = :rpc.call(node, :ets, :insert, [:bench, {newId.(), "alice"}]); :true = :ets.insert(:bench, {newId.(), "alice"}) end,
-    "cets" => fn -> :ok = :cets.insert(:cets_user, {newId.(), "alice"}) end,
-    "mnesia" => fn -> :mnesia.sync_dirty(fn -> :mnesia.write({:user, newId.(), "alice"}) end) end
+    "mnesia" => fn -> :mnesia.sync_dirty(fn -> :mnesia.write({:user, newId.(), "alice"}) end) end,
+    "cets" => fn -> :ok = :cets.insert(:cets_user, {newId.(), "alice"}) end
 #   "cets_new" => fn -> :true = :cets.insert_new(:cets_user_new, {newId.(), "alice"}) end
   },
   warmup: 2,
   time: 10,
-  parallel: 1
+  parallel: 2,
 # profile_after: true
 # profile_after: :fprof
-# profile_after: {:fprof, [sort: :own , apply_opts: [procs: [pid1, pid1_new]]]}
+  profile_after: {:fprof, [sort: :own , apply_opts: [procs: [pid1, pid1_new]]]}
 )
 #:mnesia.info()
